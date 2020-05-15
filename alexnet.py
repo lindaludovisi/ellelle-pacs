078c1cd466932c407dd6325ed832eec4d99ff768
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-05-15 18:11:42 +0200

    Update alexnet.py

--- alexnet.py
+++ alexnet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 #from torch.utils.model_zoo import load_url as load_state_dict_from_url
 from torch.hub import load_state_dict_from_url
+from torch.autograd import Function
 
 
 __all__ = ['AlexNet', 'alexnet']
